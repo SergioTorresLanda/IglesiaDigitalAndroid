dfc86f4ed52f716223157b629d8ed7d8ff0fde93
José Sebastián <sebastian.che21@gmail.com>
2023-02-02 15:41:02 -0600

    fix: se homologo la vista de oraciones

--- EAMXOraciones/src/main/java/mx/arquidiocesis/oraciones/ui/DetalleOracionFragment.kt
+++ EAMXOraciones/src/main/java/mx/arquidiocesis/oraciones/ui/DetalleOracionFragment.kt
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.arquidiocesis.oraciones.databinding.FragmentDetalleOracionBinding
 import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.fragment_detalle_oracion.*
 import mx.arquidiocesis.eamxcommonutils.application.AppMyConstants
 import mx.arquidiocesis.eamxcommonutils.base.FragmentBase
 import mx.arquidiocesis.eamxcommonutils.common.EAMXHome
@@ -43,7 +44,7 @@ class DetalleOracionFragment : FragmentBase() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        btnBack.setOnClickListener { activity?.onBackPressed() }
         val idOracion = requireArguments().getInt("idOracion")
         oracionDetalleViewModel.obtenerDetalle(idOracion)
         showLoader()
@@ -57,18 +58,21 @@ class DetalleOracionFragment : FragmentBase() {
             callBack.showToolbar(true, oracionModel.name)
             binding.tvTitulos.text = oracionModel.name
             Glide.with(requireContext()).load(Uri.parse(oracionModel.image_url))
-                .apply(RequestOptions().override(700, 373).centerCrop())
+                .apply(RequestOptions()
+                    //.override(700, 373)
+                    //.centerCrop()
+                )
                 .into(binding.ivDetalleOracion)
-/* val oracionAdapter = OracionDetalleAdapter(oracionModel.similars, requireContext()) {
-    limpiarPantalla()
-    showLoader()
-    oracionDetalleViewModel.obtenerDetalle(it.id)
-}
- binding.rvDetalleOracion.apply {
-    layoutManager = LirLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
-    adapter = oracionAdapter
-    binding.rvDetalleOracion.visibility = View.VISIBLE
-}*/
+            /* val oracionAdapter = OracionDetalleAdapter(oracionModel.similars, requireContext()) {
+                limpiarPantalla()
+                showLoader()
+                oracionDetalleViewModel.obtenerDetalle(it.id)
+            }
+             binding.rvDetalleOracion.apply {
+                layoutManager = LirLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
+                adapter = oracionAdapter
+                binding.rvDetalleOracion.visibility = View.VISIBLE
+            }*/
             hideLoader()
         }
         oracionDetalleViewModel.errorResponse.observe(viewLifecycleOwner) {
