27f0630263f34633b499ea4d458e94224e49d7cf
José Sebastián <sebastian.che21@gmail.com>
2023-02-17 03:29:35 -0600

    fix: se agrego código faltante

--- EAMXCommonUtils/src/main/java/mx/arquidiocesis/eamxcommonutils/util/EAMXExtensionsView.kt
+++ EAMXCommonUtils/src/main/java/mx/arquidiocesis/eamxcommonutils/util/EAMXExtensionsView.kt
@@ -1,6 +1,12 @@
 package mx.arquidiocesis.eamxcommonutils.util
 
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.marginBottom
+import androidx.core.view.marginLeft
+import androidx.core.view.marginRight
+import androidx.core.view.marginTop
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModel
@@ -8,4 +14,15 @@ import androidx.lifecycle.ViewModelProvider
 
 inline fun AppCompatActivity.showLoader() {
 
+}
+
+fun View.setMargins(
+    left: Int = this.marginLeft,
+    top: Int = this.marginTop,
+    right: Int = this.marginRight,
+    bottom: Int = this.marginBottom,
+) {
+    layoutParams = (layoutParams as ViewGroup.MarginLayoutParams).apply {
+        setMargins(left, top, right, bottom)
+    }
 }
