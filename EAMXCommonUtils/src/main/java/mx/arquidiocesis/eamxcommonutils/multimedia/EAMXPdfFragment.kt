5ae68d803e830ca111d770dbad1d5ab8054d0031
José Sebastián <sebastian.che21@gmail.com>
2023-02-21 15:51:30 -0600

    fix: se cambio componente de pdf

--- EAMXCommonUtils/src/main/java/mx/arquidiocesis/eamxcommonutils/multimedia/EAMXPdfFragment.kt
+++ EAMXCommonUtils/src/main/java/mx/arquidiocesis/eamxcommonutils/multimedia/EAMXPdfFragment.kt
@@ -1,53 +1,114 @@
 package mx.arquidiocesis.eamxcommonutils.multimedia
 
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.widget.LinearLayout
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
-import com.google.common.reflect.Reflection.getPackageName
+import es.voghdev.pdfviewpager.library.PDFViewPager
+import es.voghdev.pdfviewpager.library.adapter.PDFPagerAdapter
+import es.voghdev.pdfviewpager.library.remote.DownloadFile
+import es.voghdev.pdfviewpager.library.remote.DownloadFileUrlConnectionImpl
+import es.voghdev.pdfviewpager.library.util.FileUtil
 import mx.arquidiocesis.eamxcommonutils.R
-import mx.arquidiocesis.eamxcommonutils.base.FragmentBase
+import mx.arquidiocesis.eamxcommonutils.customui.alert.ProgressDialog
+import mx.arquidiocesis.eamxcommonutils.customui.alert.UtilAlert
 import mx.arquidiocesis.eamxcommonutils.databinding.FragmentPdfBinding
+import java.io.File
+import java.util.*
 
-class EAMXPdfFragment : Fragment() {
+class EAMXPdfFragment : Fragment(), DownloadFile.Listener {
+    lateinit var binding: FragmentPdfBinding
+    private var adapter: PDFPagerAdapter? = null
+    private val dialogError by lazy {
+        UtilAlert
+            .Builder()
+            .setTitle(getString(R.string.title_dialog_warning))
+            .setMessage("Ocurrio un error al intentar cargar el recurso")
+            .setListener {
+                activity?.onBackPressed()
+            }
+            .setIsCancel(false)
+            .build()
+    }
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
-    //initView()
-        return (FragmentPdfBinding.inflate(inflater, container, false)
-            .viewPdf.apply {
-                val pdf = arguments?.getString("pdf")
+        binding = FragmentPdfBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-                settings.setSupportZoom(true)
-                settings.javaScriptEnabled
-                settings.allowFileAccess
-                webViewClient = object :WebViewClient(){
-                    override fun shouldOverrideUrlLoading(
-                        view: WebView?,
-                        urlpdf: String
-                    ): Boolean {
-                        loadUrl(urlpdf)
-                        return true
-                    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.getString("pdf")?.let(::loadPDf)
+    }
+
+    private fun loadPDf(pdfUrl: String) {
+        val file = File(context?.cacheDir, FileUtil.extractFileNameFromURL(pdfUrl))
+        if (file.exists()) {
+            if (file.lastModified() != 0L) {
+                val filesModifiedDate = Calendar.getInstance()
+                filesModifiedDate.timeInMillis = file.lastModified()
+                val currentTime = Calendar.getInstance()
+                currentTime.add(Calendar.WEEK_OF_MONTH, -1)
+                //si el archivo lleva guardado menos de una semana lo muestra si no lo descarga
+                if (filesModifiedDate >= currentTime) {
+                    addPdfView(file.absolutePath)
+                    return
                 }
-                    loadUrl("https://docs.google.com/gview?embedded=true&url=" + pdf)
-
-            }).rootView
-    }
-    private fun initView() {
-        //showLoader("lOADER")
-    }
-    override fun onDestroyView() {
-        super.onDestroyView()
-        //callBack.restoreToolbar()
+            }
+        }
+        dialogProgress.show(childFragmentManager)
+        DownloadFileUrlConnectionImpl(context, Handler(), this)
+            .download(pdfUrl, file.absolutePath)
+        file.setLastModified(Calendar.getInstance().timeInMillis)
     }
 
+    private fun addPdfView(path: String) {
+        if (context == null) return
+        val pdfViewPager = PDFViewPager(requireContext(), path)
+        adapter = PDFPagerAdapter(requireContext(), path)
+        pdfViewPager.adapter = this.adapter
+        addView(pdfViewPager)
+    }
+
+    private fun addView(view: View) {
+        binding.lPdf.removeAllViews()
+        binding.lPdf.addView(
+            view, LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+            )
+        )
+
+    }
+
+    private val dialogProgress by lazy {
+        ProgressDialog()
+            .setOnCancel {
+                it.dismiss()
+                activity?.onBackPressed()
+            }
+    }
+
+    override fun onSuccess(url: String?, destinationPath: String) {
+        addPdfView(destinationPath)
+        dialogProgress.dismiss()
+    }
+
+    override fun onFailure(e: Exception?) {
+        dialogProgress.dismiss()
+        dialogError.show(childFragmentManager)
+    }
+
+    override fun onProgressUpdate(progress: Int, total: Int) {
+        dialogProgress.setProgress(progress * 100 / total)
+    }
 }
