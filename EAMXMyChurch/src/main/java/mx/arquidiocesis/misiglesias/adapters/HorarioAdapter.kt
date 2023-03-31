bbd07a85e2c37b268ee3d1e47771ee82f6845114
José Sebastián <sebastian.che21@gmail.com>
2023-03-30 21:04:58 -0600

    feat: Ajuste de diseño de horarios

--- EAMXMyChurch/src/main/java/mx/arquidiocesis/misiglesias/adapters/HorarioAdapter.kt
+++ EAMXMyChurch/src/main/java/mx/arquidiocesis/misiglesias/adapters/HorarioAdapter.kt
@@ -59,9 +59,9 @@ class HorarioAdapter(
             //binding.tvServicio.text = service.type.name
             binding.tvDia.visibility = View.GONE
 
-            binding.tvServicio.text = PublicFunctions().obtenerDias(item.days)
+            binding.tvHorario.text = PublicFunctions().obtenerDias(item.days)
             if(!item.hour_start.isNullOrEmpty()){
-                binding.tvHorario.text = "${item.hour_start}  "
+                binding.tvServicio.text = "${item.hour_start}  "
             }
            /* if(!item.hour_end.isNullOrEmpty()){
                 binding.tvHorario.text = "${binding.tvHorario.text} ${item.hour_end} "
